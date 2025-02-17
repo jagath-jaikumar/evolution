48ed0e234297d25c909621c5bfe8dbe999eaa739
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2025-02-16 22:38:56 -0500

    fix

--- evolution/evolution_game/settings_prod.py
+++ evolution/evolution_game/settings_prod.py
@@ -2,4 +2,4 @@ from evolution.evolution_game.settings import *  # noqa: F403
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "*.railway.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "healthcheck.railway.app"]
