e653c7c7cf2b047f4de8b8dc73026237cb4af4d3
lukehe147 <60271310+lukehe147@users.noreply.github.com>
2025-02-19 22:03:34 -0500

    ruff :(

--- evolution/evolution_core/base/tests/test_animal.py
+++ evolution/evolution_core/base/tests/test_animal.py
@@ -1,13 +1,13 @@
 from evolution.evolution_core.base.animal import Animal
-from evolution.evolution_core.cards.traits import *
+from evolution.evolution_core.cards.traits import TraitCard, Simplification, Carnivorous
 
 def test_my_animal():
     my_animal = Animal()
     my_animal.add_trait(TraitCard([Simplification(), Carnivorous()]),1)
     my_animal.take_food()
 
-    assert my_animal.food_req is 2
-    assert my_animal.food is 1
+    assert my_animal.food_req == 2
+    assert my_animal.food == 1
     assert my_animal.does_starve() is True
     assert my_animal.is_sheltered is False
 
