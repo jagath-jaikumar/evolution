d2924ec67938e7cc58640c81f243a048e2956a9d
lukehe147 <60271310+lukehe147@users.noreply.github.com>
2025-02-20 18:02:31 -0500

    bonus actions defined and tested

--- evolution/evolution_core/base/animal.py
+++ evolution/evolution_core/base/animal.py
@@ -1,4 +1,6 @@
-from evolution.evolution_core.cards.traits import Trait, TraitCard, FatTissue
+from evolution.evolution_core.cards.traits import (
+    Trait, FatTissue, Metamorphosis, Piracy, 
+    Homeothermy, EcosystemEngineer, Detritivore)
 import uuid
 
 class Animal:
@@ -12,79 +14,138 @@ class Animal:
         self.fat = 0
         self.fat_cap = 0
         self.score = 3
-        self.bonus_action = 0
+        self.available_bonus_action = []
     
-    def add_trait(self, card: TraitCard, position: int):
-        self.traits.append(card.traits[position])
-        self.food_req += card.traits[position].food_requirement
-        self.score += 1 + card.traits[position].food_requirement
-        if isinstance(card.traits[position], FatTissue):
+    def add_trait(self, trait_to_add: Trait): # if using TraitCard and position, utilize 'card.traits[position]'
+        self.traits.append(trait_to_add)
+        self.food_req += trait_to_add.food_requirement
+        self.score += 1 + trait_to_add.food_requirement
+        if isinstance(trait_to_add, FatTissue):
             self.fat_cap += 1
-        if (card.traits[position].is_bonus_action):
-            self.bonus_action += 1
+        if (trait_to_add.is_bonus_action):
+            self.available_bonus_action.append(trait_to_add)
+        return self
     
-    def take_shelter(self):
+    def remove_trait(self, trait_to_remove: Trait):
+        self.food_req -= trait_to_remove.food_requirement
+        self.score -= 1 + trait_to_remove.food_requirement
+        if isinstance(trait_to_remove, FatTissue):
+            self.fat_cap -= 1
+        if (trait_to_remove.is_bonus_action):
+            self.available_bonus_action.remove(trait_to_remove)
+        return self
+    
+    def gain_shelter(self):
         if (self.is_sheltered):
             raise ValueError("Cannot take shelter token: already sheltered")
         self.is_sheltered = True
+        return self
     
-    def take_food(self):
+    def gain_food(self):
         if (self.food + self.fat >= self.food_req + self.fat_cap):
             raise ValueError("Cannot take food tokens: already full with " + str(self.food) + " food/fat.")
         self.food += 1
+        return self
     
+    def lose_food(self):
+        if (self.food == 0):
+            raise ValueError("Cannot be stolen from, has no food.")
+        self.food -= 1
+        return self
+
     def successful_hunt(self):
         if (self.food + self.fat >= self.food_req + self.fat_cap):
             raise ValueError("Cannot eat another animal: already full with " + str(self.food) + " food/fat.")
         self.food += 2
+        return self
         # animals can overfeed with this; be sure to modify at end of epoch
 
-    def stolen_from(self):
-        if (self.food == 0):
-            raise ValueError("Cannot be stolen from, has no food.")
-        self.food -= 1
 
-    def metamorphosis(self, remove_trait: Trait, card: TraitCard, position: int):
-        if not any(isinstance(remove_trait, Trait) for trait in self.traits):
-            raise ValueError("Cannot remove selected trait, does not exist.")
-        self.traits.remove(remove_trait)
-        self.traits.append(card.traits[position])
+    '''
+    NOTES ON BONUS ACTIONS:
+    available_bonus_action as a list of Trait objects that can be used OPTIONALLY on your turn during areas phase and ONCE per epoch.
 
-        self.food_req -= remove_trait.food_requirement
-        self.score -= 1 + remove_trait.food_requirement
-        if isinstance(remove_trait, FatTissue):
-            self.fat_cap -= 1
-        if (remove_trait.is_bonus_action):
-            self.bonus_action -= 1
+    CURRENT BONUS ACTIONS:
+    Metamorphosis
+    Piracy: use via gain_food
+    Homeothermy
+    Ecosystem Engineer
+    Detritivore
 
-        self.food_req += card.traits[position].food_requirement
-        self.score += 1 + card.traits[position].food_requirement
-        if isinstance(card.traits[position], FatTissue):
-            self.fat_cap += 1
-        if (card.traits[position].is_bonus_action):
-            self.bonus_action += 1
-
-    def bonus_action(self):
-        '''
-        OPTIONAL AND CONDITIONAL:
-        Hibernation (extinction phase if does_starve() true)
-        Hoofed (when taking food)
-        Intellect (when attacking)
-        Repelling (when attacked), Tail Loss (when attacked), Angler Fish (when bald and attacked), Ink Cloud (when attacked)
+    OPTIONAL AND CONDITIONAL:
+    Hibernation (extinction phase if does_starve() true)
+    Hoofed (when taking food)
+    Intellect (when attacking)
+    Repelling (when attacked), Tail Loss (when attacked), Angler Fish (when bald and attacked), Ink Cloud (when attacked)
+    
+    OTHER:
+    Poisonous (if poisonous animal is eaten, triggers on predator during extinction phase)
+    Viviparous (triggers once animal is fed)
+    
+    '''
+    def use_metamorphosis(self, trait_to_remove: Trait, trait_to_add):
+        if not any(isinstance(trait, Metamorphosis) for trait in self.traits):
+            raise ValueError("Animal does not have Metamorphosis; this action cannot be taken.")
         
-        OPTIONAL TO BE USED ON YOUR TURN:
-        Metamorphosis
-        Piracy: use via take_food
-        Homeothermy
-        Ecosystem Engineer
-        Detritivore
+        if not any(isinstance(trait, type(trait_to_remove)) for trait in self.traits):
+            raise ValueError("Cannot remove selected trait with metamorphosis, selected trait not exist.")
         
-        '''
-        return
+        self.remove_trait(Metamorphosis) # TODO: Metamorphosis is discarded once used.... right??
+        self.remove_trait(trait_to_remove)
+        self.add_trait(trait_to_add)
+
+    def use_piracy(self, target: "Animal"):
+        if not any(isinstance(trait, Piracy) for trait in self.traits):
+            raise ValueError("Animal does not have Piracy; this action cannot be taken.")
+        if not any(isinstance(trait, Piracy) for trait in self.available_bonus_action):
+            raise ValueError("Animal cannot use Piracy again; this action cannot be taken.")
+        
+        self.available_bonus_action.remove(Piracy())
+        self.gain_food()
+        target.lose_food()
+    
+    def use_homeothermy(self):
+        if not any(isinstance(trait, Homeothermy) for trait in self.traits):
+            raise ValueError("Animal does not have Homeothermy; this action cannot be taken.")
+        if not any(isinstance(trait, Homeothermy) for trait in self.available_bonus_action):
+            raise ValueError("Animal cannot use Homeothermy again; this action cannot be taken.")
+        
+        self.available_bonus_action.remove(Homeothermy())
+        # TODO: Implement mechanic in game state(?), give this animal another action
+
+    def use_ecosystem_engineer(self):
+        if not any(isinstance(trait, EcosystemEngineer) for trait in self.traits):
+            raise ValueError("Animal does not have Ecosystem Engineer; this action cannot be taken.")
+        if not any(isinstance(trait, EcosystemEngineer) for trait in self.available_bonus_action):
+            raise ValueError("Animal cannot use Ecosystem Engineer again; this action cannot be taken.")
+        
+        self.available_bonus_action.remove(EcosystemEngineer())
+        # TODO: Implement mechanic in game state(?), add food and shelter to the game board (does it matter where?)
+
+    def use_detritivore(self):
+        if not any(isinstance(trait, Detritivore) for trait in self.traits):
+            raise ValueError("Animal does not have Detritivore; this action cannot be taken.")
+        if not any(isinstance(trait, Detritivore) for trait in self.available_bonus_action):
+            raise ValueError("Animal cannot use Detritivore again; this action cannot be taken.")
+        
+        self.available_bonus_action.remove(Detritivore())
+        self.gain_food()
     
     def does_starve(self):
         return (self.food_req - self.food > 0)
 
+    def reset_for_next_epoch(self):
+        self.available_bonus_action = [trait for trait in self.traits if trait.is_bonus_action]
+        self.is_sheltered = False
+
+        if (self.food + self.fat - self.food_req >= self.fat_cap):
+            self.food = self.fat_cap
+        elif (0 < self.food + self.fat - self.food_req < self.fat_cap):
+            self.food = self.food + self.fat - self.food_req
+        else:
+            self.food = 0
+
+        self.fat = 0
 
 '''
 Apply traits
--- evolution/evolution_core/base/tests/test_animal.py
+++ evolution/evolution_core/base/tests/test_animal.py
@@ -1,52 +1,86 @@
 import pytest
 from evolution.evolution_core.base.animal import Animal
 from evolution.evolution_core.cards.traits import (
-    TraitCard, Simplification, Carnivorous, Cosmopolitan, 
-    Swimming, HighBodyWeight, Partnership, FatTissue)
+    Carnivorous, Cosmopolitan, HighBodyWeight, 
+    FatTissue, Metamorphosis, Piracy, Detritivore)
 
 def test_add_traits():
-    my_animal = Animal()
-    my_animal.add_trait(TraitCard([Simplification(), Carnivorous()]),1)
-    my_animal.add_trait(TraitCard([Cosmopolitan(), Swimming()]),0)
+    my_animal = Animal().add_trait(Carnivorous()).add_trait(Cosmopolitan())
 
     assert my_animal.food_req == 2
     assert my_animal.score == 6
 
 def test_food_intake():
-    my_animal = Animal()
-    my_animal.add_trait(TraitCard([Simplification(), Carnivorous()]),1)
-    my_animal.take_food()
+    my_animal = Animal().add_trait(Carnivorous())
+    my_animal.gain_food()
 
     assert my_animal.food == 1
     assert my_animal.does_starve() is True
 
 def test_shelter_and_hunting():
-    my_animal = Animal()
-    my_animal.add_trait(TraitCard([Simplification(), Carnivorous()]), 1)
-    my_animal.take_food()
-    my_animal.take_shelter()
+    my_animal = Animal().add_trait(Carnivorous())
+    my_animal.gain_food()
+    my_animal.gain_shelter()
     my_animal.successful_hunt()
 
     assert my_animal.does_starve() is False
     assert my_animal.is_sheltered is True
 
 def test_metamorphosis():
-    my_animal = Animal()
-    my_animal.add_trait(TraitCard([Cosmopolitan(), Swimming()]),0)
-    my_animal.metamorphosis(Cosmopolitan(), TraitCard([Simplification(), HighBodyWeight()]), 1) # Exchange Cosmopolitan for High Body Weight
+    my_animal = Animal().add_trait(Cosmopolitan()).add_trait(Metamorphosis())
+    my_animal.use_metamorphosis(Cosmopolitan(), HighBodyWeight()) # Exchange Cosmopolitan for High Body Weight
 
     assert my_animal.score == 5
     assert my_animal.food_req == 2
 
-    my_animal.metamorphosis(HighBodyWeight(), TraitCard([Partnership(), FatTissue()]), 1) # Exchanges High Body Weight with Fat Tissue
+    my_animal.add_trait(Metamorphosis())
+    my_animal.use_metamorphosis(HighBodyWeight(), FatTissue()) # Exchanges High Body Weight with Fat Tissue
 
     assert my_animal.fat_cap == 1
 
-def test_invalid_mteamorphosis():
-    my_animal = Animal()
-    my_animal.add_trait(TraitCard([Cosmopolitan(), Swimming()]),0)
+def test_invalid_metamorphosis():
+    my_animal = Animal().add_trait(Cosmopolitan())
 
-    with pytest.raises(ValueError, match="list.remove\\(x\\): x not in list"):
-        my_animal.metamorphosis(Carnivorous(), TraitCard([Partnership(), FatTissue()]), 1)
+    with pytest.raises(ValueError, match="Animal does not have Metamorphosis; this action cannot be taken."):
+        my_animal.use_metamorphosis(Carnivorous(), FatTissue())
+    
+    assert my_animal.score == 4
 
+def test_piracy_valid_and_invalid():
+    pirate_animal = Animal().add_trait(Piracy())
+    target_animal = Animal()
+    target_animal.food = 2
+
+    pirate_animal.use_piracy(target_animal)
+
+    assert pirate_animal.food == 1, "Pirate animal should have gained one food."
+    assert target_animal.food == 1, "Target animal should have lost one food."
+
+    with pytest.raises(ValueError, match="Animal cannot use Piracy again; this action cannot be taken."):
+        pirate_animal.use_piracy(target_animal)
+        
+def test_detritivore_valid_and_invalid():
+    detritivore_animal = Animal().add_trait(Detritivore())
+    
+    assert detritivore_animal.does_starve() is True
+
+    detritivore_animal.use_detritivore()
+
+    assert detritivore_animal.does_starve() is False
+
+    with pytest.raises(ValueError, match="Animal cannot use Detritivore again; this action cannot be taken."):
+        detritivore_animal.use_detritivore()
+
+def test_reset_for_next_epoch():
+    detritivore_animal = Animal().add_trait(Detritivore()).add_trait(FatTissue())
+    detritivore_animal.food = 1
+    detritivore_animal.use_detritivore()
+    detritivore_animal.reset_for_next_epoch()
+
+    assert detritivore_animal.food == 1
+
+    detritivore_animal.use_detritivore()
+
+    assert detritivore_animal.food == 2
+    
 # python -m poetry run pytest
